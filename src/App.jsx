5d0103333bc35f450f6e5038d0c2f0e94b54338b
Hossamkhatap31 <khatabhossam88@gmail.com>
2025-01-25 18:22:33 +0200

    Deploy to GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,38 @@
 
 
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-
-import './App.css'
-import Layout from './Component/Layout/Layout'
-import Home from './Component/Home/Home'
-import About from './Component/About/About';
-import Portfolio from './Component/Portfolio/Portfolio';
-import Contact from './Component/Contact/Contact';
-
-
+import "./App.css";
+import Layout from "./Component/Layout/Layout";
+import Home from "./Component/Home/Home";
+import About from "./Component/About/About";
+import Portfolio from "./Component/Portfolio/Portfolio";
+import Contact from "./Component/Contact/Contact";
+import NotFound from "./Component/NotFound/NotFound";
 
 
 function App() {
-
- 
-  
-  
-
-
-
-
-  let router =createBrowserRouter([
-    {path:"",element:<Layout /> ,children:[
-      {path:"" , element:<Home />},
-      {path:"home" , element:<Home />},
-      {path:"about" , element:<About />},
-      {path:"portfolio" , element:<Portfolio />},
-      {path:"contact" , element:<Contact />},
-      
-
-    ]}
-  ])
- 
+  // تعريف المسارات
+  let router = createBrowserRouter([
+    {
+      path: "/", // المسار الجذر
+      element: <Layout />,
+      children: [
+        { path: "/", element: <Home /> },
+        { path: "home", element: <Home /> },
+        { path: "about", element: <About /> },
+        { path: "portfolio", element: <Portfolio /> },
+        { path: "contact", element: <Contact /> },
+        { path: "*", element: <NotFound /> }, // مسار للأخطاء
+      ],
+    },
+  ]);
 
   return (
     <>
-
-    <RouterProvider  router={router}/>
-   
-
-    
-
+      <RouterProvider router={router} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
